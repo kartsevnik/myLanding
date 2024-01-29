be681f6c751c6ea23bdc1eb73061aa9f02f54476
kartsevnik <kartsevnik@gmail.com>
2024-01-29 12:28:13 +0100

    add function of greatings

--- js/index.js
+++ js/index.js
@@ -53,3 +53,45 @@ class ChangeBlock {
 
 const changeTitleWorkExpirience = new ChangeBlock("blockToMove", "right-Block", "prepend", "left-Block", "prepend");
 const changeExperienceButton = new ChangeBlock("experience-button", "left-Block", "append", "right-Block", "append");
+
+function greatings() {
+    let currentDate = new Date;
+    let time = currentDate.getHours();
+    const element = document.getElementById("greatings");
+
+    switch (time) {
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+        case 10:
+        case 11:
+        case 12:
+            element.innerHTML = "Good Morning!";
+            break;
+
+        case 13:
+        case 14:
+        case 15:
+        case 16:
+        case 17:
+        case 18:
+            element.innerHTML = "Good Day!";
+            break;
+
+        case 19:
+        case 20:
+        case 21:
+        case 22:
+        case 23:
+            element.innerHTML = "Good Evening!";
+            break;
+
+        default:
+            element.innerHTML = "Good Night!";
+    }
+}
+
+window.onload = function () {
+    greatings()
+}
